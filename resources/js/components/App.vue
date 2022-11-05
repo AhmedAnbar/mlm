b99a9a11a774446aa97fed8bd2a5ef0dee8fe8b6
Ahmed anbar <ahmed.anbar@azm.dev>
2022-11-05 20:20:05 +0200

    Initial commit

---
+++ resources/js/components/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="flex flex-row h-screen justify-center">
+        <div class="basis-1/2 bg-[#05445e] ">
+            <div class="flex flex-row items-center h-screen justify-center">
+                <div class="basis-1/1">
+                    <img alt="MLM System Admin" class="align-middle" src="/img/admin/admin_login_logo.svg">
+                </div>
+            </div>
+
+        </div>
+        <div class="basis-1/2">
+            <div class="flex flex-row items-center h-screen justify-center">
+                <div class="basis-1/1  px-10">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus dolores illum iure minima nostrum possimus repellendus. Aspernatur dolor et, inventore ipsum laborum molestiae quaerat qui quidem, quis quisquam reprehenderit, voluptate.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "App"
+}
+</script>
+
+<style scoped>
+
+</style>
